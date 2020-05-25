a310c31132e0fd3d7fb33896d5fc1d998cfe423d
Renato Mintz <remintz@gmail.com>
2020-05-24 22:16:03 -0300

    working

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import curses
 
 class Computer:
-    def __init__(self):
+    def __init__(self, screen):
+        self.__screen = screen
         self.__memory = [0] * 32
+        self.__disassembly = [''] * 32
         self.__a = 0
         self.__z = False
         self.__neg = False
@@ -27,10 +30,10 @@ class Computer:
             if len(line) == 0:
                 continue
             tokens = line.split(' ')
-            print('tokens: %s' % str(tokens))
             instruction = tokens[0]
             data_low = None
             data_high = None
+            data = None
             instruction_code = self.__instructions.index(instruction)
             if instruction in ['LDAM', 'LDAI', 'STA', 'SUM', 'SUB', 'JMP', 'JZ', 'JN', 'AND', 'OR', 'XOR']:
                 data = int(tokens[1])
@@ -40,6 +43,10 @@ class Computer:
                 else:
                     data_low = data
             self.__memory[idx_memory] = instruction_code
+            disassembly = instruction
+            if data is not None:
+                disassembly = disassembly + ' ' + str(data)
+            self.__disassembly[idx_memory] = disassembly
             idx_memory += 1
             if data_low is not None:
                 self.__memory[idx_memory] = data_low
@@ -71,14 +78,15 @@ class Computer:
             self.print_status()
             pass
 
-    def print_mem(self):
-        for i in range(32):
-            print('MEM [%2d]: %5d' % (i, self.__memory[i]))
+    # def print_mem(self):
+    #     for i in range(32):
+    #         print('MEM [%2d]: %5d' % (i, self.__memory[i]))
 
     def print_status(self):
-        print('SP: %d' % self.__sp)
-        print('A: %d' % self.__a)
-        self.print_mem()
+        self.__screen.refresh(self.__memory, self.__disassembly, self.__a, self.__sp)
+        # print('SP: %d' % self.__sp)
+        # print('A: %d' % self.__a)
+        # self.print_mem()
 
     def exec_step(self):
         binary_instruction = self.__memory[self.__sp]
@@ -140,28 +148,106 @@ class Computer:
             self.__a == ~ self.__a
             self.__sp += 1
         elif instruction == 'IN':
-            value = int(input('Entre com um valor:'))
+            value = self.__screen.input()
             self.__a = value
             self.__sp += 1
         elif instruction == 'OUT':
-            print('out: %d' % self.__a)
+            self.__screen.show_output(self.__a)
             self.__sp += 1
         elif instruction == 'END':
+            self.__screen.show_finished()
             return True
         self.update_flags()
         return False
 
-c = Computer()
-c.set_program(
-        """
-        IN
-        STA 30
-        IN
-        STA 28
-        LDAM 30
-        SUM 28
-        OUT
-        END
-        """)
-c.exec()
+class Screen:
+    def __init__(self):
+        self.__stdscr = curses.initscr()
+        self.__memory = []
+        self.__registers = {}
+        curses.echo()
+        curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+
+    def __del__(self):
+        self.__stdscr.keypad(False)
+        curses.echo()
+        curses.endwin()
+
+    def show_memory(self, mem, disassembly, sp):
+        l = 1
+        c = 1
+        positions = 32
+        self.__stdscr.addstr(l, c, 'MEMORY', curses.A_BOLD)
+        l += 1
+        self.__stdscr.addstr(l, c, '      +----------+ +------+ +----------------+')
+        l += 1
+        for i in range(positions):
+            sp_indicator = ''
+            if i == sp:
+                sp_indicator = '>>'
+            s = f'{sp_indicator:2s} {i:>2} | {mem[i]:>08b} | | {mem[i]:>4d} | | {disassembly[i]:14s} |'
+            if i == sp:
+                self.__stdscr.addstr(l, c, s, curses.color_pair(1))
+            else:
+                self.__stdscr.addstr(l, c, s)
+            l += 1
+        self.__stdscr.addstr(l, c, '      +----------+ +------+ +----------------+')
+
+    def show_a(self, a):
+        l = 3
+        c = 55
+        self.__stdscr.addstr(l, c, 'ACCUMULATOR: ', curses.A_BOLD)
+        self.__stdscr.addstr(l, c + 15, f'{a:08b}b {a:4d}')
+
+    def show_output(self, output):
+        l = 5
+        c = 55
+        if output is None:
+            out = ''
+        else:
+            out = str(output)
+        self.__stdscr.addstr(l, c, 'OUTPUT: ', curses.A_BOLD)
+        self.__stdscr.addstr(l, c + 15, f'{out:15s}')
+
+    def show_finished(self):
+        self.__stdscr.addstr(36, 0, 'Execution terminated. Press any key to exit..........', curses.color_pair(2))
+        c = self.__stdscr.getch()
+
+    def input(self):
+        l = 1
+        c = 30
+        self.__stdscr.addstr(l, c, 'INPUT: ', curses.A_STANDOUT)
+        s = self.__stdscr.getstr(l, c + 10, 6)
+        self.__stdscr.addstr(l, c, ' ' * 30)
+        return int(s)
+
+    def refresh(self, mem, disassembly, a, sp):
+        self.show_a(a)
+        self.show_memory(mem, disassembly, sp)
+        self.__stdscr.refresh()
+        self.__stdscr.addstr(36, 0, 'Press Q to finish or any other key to continue: ')
+        c = self.__stdscr.getch()
+        if c == 'q' or c == 'Q':
+            exit()
+
+def main(stdscr):
+    print('main')
+    s = Screen()
+    c = Computer(s)
+    c.set_program(
+            """
+            IN
+            STA 30
+            IN
+            STA 28
+            LDAM 30
+            SUM 28
+            OUT
+            END
+            """)
+    c.exec()
+
+print('mmain')
+curses.wrapper(main)
 
